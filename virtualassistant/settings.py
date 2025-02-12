4f10aa45892f4c4f3d67170c497bd04281b7604e
Iñaki Retegi <retegi84@gmail.com>
2025-02-12 22:33:00 +0100

    redirect

--- virtualassistant/settings.py
+++ virtualassistant/settings.py
@@ -159,3 +159,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGOUT_REDIRECT_URL = 'home_app:home'
+LOGIN_REDIRECT_URL = 'home_app:home'
