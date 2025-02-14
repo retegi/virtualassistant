936310f3a277737ef5a0e17aa0ecc6bff5ebf932
olvin78 <olvind78@gmail.com>
2025-02-15 00:16:03 +0100

    Guardando cambios antes del merge

--- applications/tuasistenteinteligente/urls.py
+++ applications/tuasistenteinteligente/urls.py
@@ -6,4 +6,4 @@ urlpatterns = [
     path('chat/', views.chat_view, name='chat_view'),
     path('api/get_response/', views.get_bot_response, name='get_bot_response'),
     path('end_conversation/', views.end_conversation, name='end_conversation'),
-]s
+]
